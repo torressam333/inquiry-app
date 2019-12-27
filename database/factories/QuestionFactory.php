2f17ebd15fe024643fe7ef478bc2b02ddf3152dc
Samuel Torres <torressam333@gmail.com>
2019-12-27 15:28:38 -0700

    Prepopulate users and questions tables using model factories

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+use App\Question;
+
+$factory->define(Question::class, function (Faker $faker) {
+    return [
+        //Define db columns you want fake data generated for
+        'title'=> rtrim($faker->sentence(rand(5,10)), "."),
+        'body' => $faker->paragraphs(rand(3,7), true),
+        'views' => rand(0,11),
+        'answers' => rand(0,11),
+        'votes' => rand(-4,11),
+    ];
+});
