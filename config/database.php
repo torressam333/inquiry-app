4de7b6ce8bd0ec45ade18e22143e698f741ea09c
Samuel Torres <torressam333@gmail.com>
2020-01-22 09:03:19 -0700

    Display all questions

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => 'InnoDB',
+            'engine' => 'null',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
@@ -75,6 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
+            'version' => 8,
             'sslmode' => 'prefer',
         ],
 
