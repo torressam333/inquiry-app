c985c0a5930aa77d1d985238675794f418747974
Samuel Torres <torressam333@gmail.com>
2020-05-05 06:31:50 -0700

    Implement the ability to view individual questions

--- app/Question.php
+++ app/Question.php
@@ -34,7 +34,7 @@ class Question extends Model
 
     public function getUrlAttribute()
     {
-        return route('questions.show', $this->id);
+        return route('questions.show', $this->slug);
     }
 
     public function getCreatedDateAttribute()
@@ -56,5 +56,10 @@ class Question extends Model
             return "answered";
         }
         return "unanswered";
+    }
+
+    public function getBodyHtmlAttribute()
+    {
+        return \Parsedown::instance()->text($this->body);
     }
 }
