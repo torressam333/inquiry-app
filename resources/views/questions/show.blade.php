13c2e32d6da1df5a763fa98266940a31b2a05f31
Samuel Torres <torressam333@gmail.com>
2020-05-14 14:37:11 -0700

    Add created_date mutator in Answer model

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -21,6 +21,7 @@
                 </div>
             </div>
         </div>
+
         <div class="row mt-4">
             <div class="col-md-12">
                 <div class="card">
@@ -35,6 +36,17 @@
                             <div class="media">
                                 <div class="media-body">
                                     {!! $answer->body_html !!}
+                                    <div class="float-right">
+                                        <span class="text-muted">Answered {{$answer->created_date}}</span>
+                                        <div class="media">
+                                            <a href="{{$answer->user->url}}" class="pr-2">
+                                                <img src="{{ $answer->user->avatar }}" alt="">
+                                            </a>
+                                            <div class="media-body">
+                                                <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <hr>
