b18529dd3659e8ac8cdd725dc85a2b5626a27d2f
Samuel Torres <torressam333@gmail.com>
2020-05-15 15:41:44 -0700

    Refactor and update application. Add votes counter information for each question.

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,26 +5,43 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">
-                        <div class="d-flex align-items-center">
-                            <h1>{{$question->title}}</h1>
-                            <div class="ml-auto">
-                                <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to all
-                                    Questions</a>
+                    <div class="card-body">
+                        <div class="card-title">
+                            <div class="d-flex align-items-center">
+                                <h1>{{$question->title}}</h1>
+                                <div class="ml-auto">
+                                    <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to all
+                                        Questions</a>
+                                </div>
                             </div>
                         </div>
-
-                    </div>
-                    <div class="card-body">
-                        {!! $question->body_html !!}
-                        <div class="float-right">
-                            <span class="text-muted">Asked {{$question->created_date}}</span>
-                            <div class="media mt-2">
-                                <a href="{{$question->user->url}}" class="pr-2">
-                                    <img src="{{$question->user->avatar}}" alt="">
+                        <hr>
+                        <div class="media">
+                            <div class="d-flex flex-column .vote-controls">
+                                <a title="This question is useful" class="vote-up">
+                                    Vote up
                                 </a>
-                                <div class="media-body mt-1">
-                                    <a href="{{$question->user->url}}">{{$question->user->name}}</a>
+                                <span class="votes-count">1234</span>
+                                <a title="This question is not useful" class="vote-down off">
+                                    Vote down
+                                </a>
+                                <a title="Click to mark as favorite question (click again to undo)" class="favorite">
+                                    Favorite Question
+                                    <span class="favorites-count">1234</span>
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                {!! $question->body_html !!}
+                                <div class="float-right">
+                                    <span class="text-muted">Asked {{$question->created_date}}</span>
+                                    <div class="media mt-2">
+                                        <a href="{{$question->user->url}}" class="pr-2">
+                                            <img src="{{$question->user->avatar}}" alt="">
+                                        </a>
+                                        <div class="media-body mt-1">
+                                            <a href="{{$question->user->url}}">{{$question->user->name}}</a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
