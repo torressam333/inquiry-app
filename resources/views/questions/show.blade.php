42fbe870dd583d60a9a60248650844891f9624dd
Samuel Torres <torressam333@gmail.com>
2020-05-16 08:03:42 -0700

    Add styling to vote controls

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -8,38 +8,39 @@
                     <div class="card-body">
                         <div class="card-title">
                             <div class="d-flex align-items-center">
-                                <h1>{{$question->title}}</h1>
+                                <h1>{{ $question->title }}</h1>
                                 <div class="ml-auto">
-                                    <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to all
-                                        Questions</a>
+                                    <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
                                 </div>
                             </div>
                         </div>
+
                         <hr>
+
                         <div class="media">
-                            <div class="d-flex flex-column .vote-controls">
+                            <div class="d-fex flex-column vote-controls">
                                 <a title="This question is useful" class="vote-up">
-                                    Vote up
+                                    <i class="fas fa-caret-up fa-3x"></i>
                                 </a>
-                                <span class="votes-count">1234</span>
+                                <span class="votes-count">1230</span>
                                 <a title="This question is not useful" class="vote-down off">
-                                    Vote down
+                                    <i class="fas fa-caret-down fa-3x"></i>
                                 </a>
-                                <a title="Click to mark as favorite question (click again to undo)" class="favorite">
-                                    Favorite Question
-                                    <span class="favorites-count">1234</span>
+                                <a title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                                    <i class="fas fa-star fa-lg"></i>
+                                    <span class="favorites-count">123</span>
                                 </a>
                             </div>
                             <div class="media-body">
                                 {!! $question->body_html !!}
                                 <div class="float-right">
-                                    <span class="text-muted">Asked {{$question->created_date}}</span>
+                                    <span class="text-muted">Answered {{ $question->created_date }}</span>
                                     <div class="media mt-2">
-                                        <a href="{{$question->user->url}}" class="pr-2">
-                                            <img src="{{$question->user->avatar}}" alt="">
+                                        <a href="{{ $question->user->url }}" class="pr-2">
+                                            <img src="{{ $question->user->avatar }}">
                                         </a>
                                         <div class="media-body mt-1">
-                                            <a href="{{$question->user->url}}">{{$question->user->name}}</a>
+                                            <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                         </div>
                                     </div>
                                 </div>
@@ -49,29 +50,38 @@
                 </div>
             </div>
         </div>
-
         <div class="row mt-4">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
                         <div class="card-title">
-                            <h2>{{$question->answers_count . ' '.
-                                    str_plural('Answer', $question->answers_count)}}</h2>
+                            <h2>{{ $question->answers_count . " " . str_plural('Answer', $question->answers_count) }}</h2>
                         </div>
                         <hr>
-                        {{--Display All Answers--}}
-                        @foreach($question->answers as $answer)
+                        @foreach ($question->answers as $answer)
                             <div class="media">
+                                <div class="d-fex flex-column vote-controls">
+                                    <a title="This answer is useful" class="vote-up">
+                                        <i class="fas fa-caret-up fa-3x"></i>
+                                    </a>
+                                    <span class="votes-count">1230</span>
+                                    <a title="This answer is not useful" class="vote-down off">
+                                        <i class="fas fa-caret-down fa-3x"></i>
+                                    </a>
+                                    <a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                                        <i class="fas fa-check fa-2x"></i>
+                                    </a>
+                                </div>
                                 <div class="media-body">
                                     {!! $answer->body_html !!}
                                     <div class="float-right">
-                                        <span class="text-muted">Answered {{$answer->created_date}}</span>
+                                        <span class="text-muted">Answered {{ $answer->created_date }}</span>
                                         <div class="media mt-2">
-                                            <a href="{{$answer->user->url}}" class="pr-2">
-                                                <img src="{{ $answer->user->avatar }}" alt="">
+                                            <a href="{{ $answer->user->url }}" class="pr-2">
+                                                <img src="{{ $answer->user->avatar }}">
                                             </a>
                                             <div class="media-body mt-1">
-                                                <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
+                                                <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
                                             </div>
                                         </div>
                                     </div>
