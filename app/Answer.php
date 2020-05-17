03d77fc3236ed724173e9789054c42abd7236990
Samuel Torres <torressam333@gmail.com>
2020-05-17 08:15:43 -0700

    Implement functionality to decrement answer count if answer is deleted

--- app/Answer.php
+++ app/Answer.php
@@ -40,5 +40,9 @@ class Answer extends Model
         static::created(function($answer){
             $answer->question->increment('answers_count');
         });
+
+        static::deleted(function ($answer){
+            $answer->question->decrement('answers_count');
+        });
     }
 }
