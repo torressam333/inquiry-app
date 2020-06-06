6a235b457bccc5c8b65700447a85cb3fae75fe07
Samuel Torres <torressam333@gmail.com>
2020-06-06 12:38:20 -0700

    Create votables table in order to vote for questions and answers

---
+++ database/migrations/2020_06_06_193203_create_voteables_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVoteablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('voteables', function (Blueprint $table) {
+            $table->unsignedInteger('user_id')->comment('Which user casted a vote');
+            $table->unsignedInteger('votable_id')->comment('Either a question or answer ID');
+            $table->string('voteable_type')->comment('Types include: question or answer');
+            $table->tinyInteger('vote')->comment('Two possible values (1 for vote up, -1 for vote down');
+            $table->timestamps();
+            $table->unique(['user_id', 'votable_id', 'voteable_type'])
+                ->comment('Allow user to only vote once per question or answer');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('voteables');
+    }
+}
