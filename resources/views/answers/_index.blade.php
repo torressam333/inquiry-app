6640962a3cb04ed4e1828703611c8e593d4cf13a
Samuel Torres <torressam333@gmail.com>
2020-06-25 13:27:22 -0700

    Separate views into modular/generic files

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -42,7 +42,7 @@
                             <a title="Mark this answer as best answer"
                                class="{{$answer->status}} mt-2"
                                onclick="event.preventDefault();
-                               document.getElementById('accept-answer-{{$answer->id}}').submit();"
+                                   document.getElementById('accept-answer-{{$answer->id}}').submit();"
                             >
                                 <i class="fas fa-check fa-2x"></i>
                             </a>
@@ -69,28 +69,27 @@
                                 <div class="col-4">
                                     <div class="ml-auto">
                                         @can ('update', $answer)
-                                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}"
+                                               class="btn btn-sm btn-outline-info">Edit</a>
                                         @endcan
                                         @can ('delete', $answer)
-                                            <form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
+                                            <form class="form-delete" method="post"
+                                                  action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
                                                 @method('DELETE')
                                                 @csrf
-                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                                <button type="submit" class="btn btn-sm btn-outline-danger"
+                                                        onclick="return confirm('Are you sure?')">Delete
+                                                </button>
                                             </form>
                                         @endcan
                                     </div>
                                 </div>
                                 <div class="col-4"></div>
                                 <div class="col-4">
-                                    <span class="text-muted">Answered {{ $answer->created_date }}</span>
-                                    <div class="media mt-2">
-                                        <a href="{{ $answer->user->url }}" class="pr-2">
-                                            <img src="{{ $answer->user->avatar }}">
-                                        </a>
-                                        <div class="media-body mt-1">
-                                            <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
-                                        </div>
-                                    </div>
+                                    @include('shared._author', [
+                                       'model' => $answer,
+                                       'label' => 'answered'
+                                    ])
                                 </div>
                             </div>
                         </div>
