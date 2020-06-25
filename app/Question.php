777c81c151c60a47177317cd8646eb3e27913dbe
Samuel Torres <torressam333@gmail.com>
2020-06-25 15:52:32 -0700

    Implement purifier clean method on question and answer text fields

--- app/Question.php
+++ app/Question.php
@@ -55,7 +55,7 @@ class Question extends Model
 
     public function getBodyHtmlAttribute()
     {
-        return $this->bodyHtml();
+        return clean($this->bodyHtml());
     }
 
     public function answers(){
