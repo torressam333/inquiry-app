2b3401f7752c5ce3e1b02c157181822bc38bec72
Samuel Torres <torressam333@gmail.com>
2020-06-26 08:03:24 -0700

    Add conditional logic for displaying answers to questions

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -1,81 +1,88 @@
-<div class="row mt-4">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-body">
-                <div class="card-title">
-                    <h2>{{ $answersCount . " " . str_plural('Answer', $answersCount) }}</h2>
-                </div>
-                <hr>
-                @include ('layouts._messages')
+@if($answersCount > 0)
+    <div class="row mt-4">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2>{{ $answersCount . " " . str_plural('Answer', $answersCount) }}</h2>
+                    </div>
+                    <hr>
+                    @include ('layouts._messages')
 
-                @foreach ($answers as $answer)
-                    <div class="media">
-                        <div class="d-fex flex-column vote-controls">
-                            <a title="This answer is useful"
-                               class="vote-up{{Auth::guest() ? 'off' : ''}}"
-                               onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{$answer->id}}').submit();"
-                            >
-                                <i class="fas fa-caret-up fa-3x"></i>
-                            </a>
-                            <form
-                                id="up-vote-answer-{{ $answer->id }}"
-                                action="/answers/{{$answer->id}}/vote"
-                                method="POST">
-                                @csrf
-                                <input type="hidden" name="vote" value="1">
-                            </form>
-                            <span class="votes-count">{{$answer->votes_count}}</span>
-                            <a title="This answer is not useful"
-                               class="vote-down{{Auth::guest() ? 'off' : ''}}"
-                               onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{$answer->id}}').submit();"
-                            >
-                                <i class="fas fa-caret-down fa-3x"></i>
-                            </a>
-                            <form
-                                id="down-vote-answer-{{ $answer->id }}"
-                                action="/answers/{{$answer->id}}/vote"
-                                method="POST">
-                                @csrf
-                                <input type="hidden" name="vote" value="-1">
-                            </form>
-                            @include('shared._accept', [
-                                 'model' => $answer,
-                             ])
-                        </div>
-                        <div class="media-body">
-                            {!! $answer->body_html !!}
-                            <div class="row">
-                                <div class="col-4">
-                                    <div class="ml-auto">
-                                        @can ('update', $answer)
-                                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}"
-                                               class="btn btn-sm btn-outline-info">Edit</a>
-                                        @endcan
-                                        @can ('delete', $answer)
-                                            <form class="form-delete" method="post"
-                                                  action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button type="submit" class="btn btn-sm btn-outline-danger"
-                                                        onclick="return confirm('Are you sure?')">Delete
-                                                </button>
-                                            </form>
-                                        @endcan
+                    @foreach ($answers as $answer)
+                        <div class="media">
+                            <div class="d-fex flex-column vote-controls">
+                                <a title="This answer is useful"
+                                   class="vote-up{{Auth::guest() ? 'off' : ''}}"
+                                   onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{$answer->id}}').submit();"
+                                >
+                                    <i class="fas fa-caret-up fa-3x"></i>
+                                </a>
+                                <form
+                                    id="up-vote-answer-{{ $answer->id }}"
+                                    action="/answers/{{$answer->id}}/vote"
+                                    method="POST">
+                                    @csrf
+                                    <input type="hidden" name="vote" value="1">
+                                </form>
+                                <span class="votes-count">{{$answer->votes_count}}</span>
+                                <a title="This answer is not useful"
+                                   class="vote-down{{Auth::guest() ? 'off' : ''}}"
+                                   onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{$answer->id}}').submit();"
+                                >
+                                    <i class="fas fa-caret-down fa-3x"></i>
+                                </a>
+                                <form
+                                    id="down-vote-answer-{{ $answer->id }}"
+                                    action="/answers/{{$answer->id}}/vote"
+                                    method="POST">
+                                    @csrf
+                                    <input type="hidden" name="vote" value="-1">
+                                </form>
+                                @include('shared._accept', [
+                                     'model' => $answer,
+                                 ])
+                            </div>
+                            <div class="media-body">
+                                {!! $answer->body_html !!}
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="ml-auto">
+                                            @can ('update', $answer)
+                                                <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}"
+                                                   class="btn btn-sm btn-outline-info">Edit</a>
+                                            @endcan
+                                            @can ('delete', $answer)
+                                                <form class="form-delete" method="post"
+                                                      action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-sm btn-outline-danger"
+                                                            onclick="return confirm('Are you sure?')">Delete
+                                                    </button>
+                                                </form>
+                                            @endcan
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-4"></div>
-                                <div class="col-4">
-                                    @include('shared._author', [
-                                       'model' => $answer,
-                                       'label' => 'answered'
-                                    ])
+                                    <div class="col-4"></div>
+                                    <div class="col-4">
+                                        @include('shared._author', [
+                                           'model' => $answer,
+                                           'label' => 'answered'
+                                        ])
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <hr>
-                @endforeach
+                        <hr>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
-</div>
+@else
+    <div class="card mt-2">
+        <p class="card-body pb-1">There are currently no answers to this question. Be the first to provide one!</p>
+    </div>
+@endif
+
