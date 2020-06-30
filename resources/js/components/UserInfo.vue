da981a4e71393bbf756cbd638a8373650bf65191
Samuel Torres <torressam333@gmail.com>
2020-06-30 07:20:16 -0700

    Transform author helper blade file into VUE component

---
+++ resources/js/components/UserInfo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <span class="text-muted">{{postDate}}</span>
+        <div class="media mt-2">
+            <a v-bind:href="user.url" class="pr-2">
+                <img v-bind:src="user.avatar">
+            </a>
+            <div class="media-body mt-1">
+                <a v-bind:href="user.url">{{ user.name }}</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['model', 'label'],
+
+        computed: {
+            postDate() {
+                return this.label + " " + this.model.created_date;
+            }
+        },
+
+        data(){
+            return {
+                user: this.model.user
+            }
+        }
+    }
+</script>
