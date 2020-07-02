28ff872e1f82fed8f8a2d1d428f54bd569bb4707
Samuel Torres <torressam333@gmail.com>
2020-07-02 15:45:55 -0700

    Convert question favorting into VUE component

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -15,12 +15,22 @@ class FavoritesController extends Controller
     public function store(Question $question)
     {
         $question->favorites()->attach(auth()->id());
+
+        if (request()->expectsJson()) {
+            return response()->json(null, 204);
+        }
+
         return back();
     }
 
     public function destroy(Question $question)
     {
         $question->favorites()->detach(auth()->id());
+
+        if (request()->expectsJson()) {
+            return response()->json(null, 204);
+        }
+
         return back();
     }
 }
