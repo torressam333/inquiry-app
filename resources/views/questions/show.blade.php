b15c91dc02913f157c1a38ae43ea6a46cb9dfe61
Samuel Torres <torressam333@gmail.com>
2020-08-01 07:08:57 -0700

    Initialise Questions vue component

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -2,43 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="card-title">
-                            <div class="d-flex align-items-center">
-                                <h1>{{ $question->title }}</h1>
-                                <div class="ml-auto">
-                                    <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to
-                                        all Questions</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <hr>
-
-                        <div class="media">
-                            <vote :model="{{$question}}" name="question"></vote>
-                            <div class="media-body">
-                                {!! $question->body_html !!}
-                                <div class="row">
-                                    <div class="col-4"></div>
-                                    <div class="col-4"></div>
-                                    <div class="col-4">
-                                        @include('shared._author', [
-                                             'model' => $question,
-                                             'label' => 'asked'
-                                         ])
-                                        <user-info v-bind:model="{{$question}}" label="Asked"></user-info>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+       <question :question="{{$question}}"></question>
         {{--Specify question property and pass quesiton instance to it--}}
         <answers :question="{{$question}}"></answers>
     </div>
