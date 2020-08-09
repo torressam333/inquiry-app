366f1d6b56b2b668b5f611e1134cffbcc8242509
Samuel Torres <torressam333@gmail.com>
2020-08-09 07:46:36 -0700

    Add endpoint for oauth token

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function getToken(Request $request)
+    {
+        //Password grant tokens using oauth token endpoint
+        $request->request->add([
+            'grant_type' => 'password',
+            'client_id' => 4,
+            'client_secret' => '5EKTXiXGKlwZUvoHfZCg8MUVE79TPqG27C6vsxwq',
+            'username' => $request->username,
+            'password' => $request->password,
+        ]);
+
+        //Returns the request object
+        $requestToken = Request::create(env('APP_URL'). '/oauth/token', 'post');
     }
 }
