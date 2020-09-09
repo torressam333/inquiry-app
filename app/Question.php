ade740e4e62f1f3005a3b332090fe00c0f34a3e5
Samuel Torres <torressam333@gmail.com>
2020-09-09 06:41:56 -0700

    Fix api authentication in Question model

--- app/Question.php
+++ app/Question.php
@@ -76,7 +76,7 @@ class Question extends Model
 
     public function isFavorited()
     {
-        return $this->favorites()->where('user_id', auth()->id())->count() > 0;
+        return $this->favorites()->where('user_id', auth('api')->id())->count() > 0;
     }
 
     public function getIsFavoritedAttribute()
